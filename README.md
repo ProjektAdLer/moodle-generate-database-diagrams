8b38a7b3545ef55f447012739afe56aa54021961
Glutamat42 <markus.heck13396@gmx.de>
2023-09-19 23:56:56 +0200

    add script, README.md and a not yet tested workflow file

---
+++ README.md
@@ -0,0 +1,55 @@
+# About this project
+The problem: When moodle designed their database system most popular database systems did not support foreign keys. 
+Therefore, they decided to implement them software side in moodle.
+As of moodle 4.2 this is still the case.
+As a result it is not possible to automatically generate useful database diagrams for moodle databases as they require foreign keys to show relations between tables.
+
+The solution: This repository contains a script that creates rudimentary SQL CREATE TABLE statements for all tables in the moodle database. 
+It implements just enough to create database diagrams as this is the only target of this script.
+It is very likely that the created database will not work with moodle.
+
+## How to use
+The following assumes you are using WSL. It should work like that on linux as well.
+
+You need a database running on your machine with the following configuration. 
+
+db-system: mariadb \
+host: localhost \
+port: 3312 \
+user: root \
+password: a \
+
+It is hardcoded in the script, but you should be able to change that easily.
+You can use the following docker-compose file to start a database with the correct configuration:
+```yaml
+version: '3'
+services:
+  db_moodle:
+    image: docker.io/bitnami/mariadb:10.6
+    ports:
+      - 3312:3306
+    environment:
+      MARIADB_USER: blub
+      MARIADB_PASSWORD: blub
+      MARIADB_ROOT_PASSWORD: a
+      MARIADB_DATABASE: blub
+      MARIADB_CHARACTER_SET: utf8mb4
+      MARIADB_COLLATE: utf8mb4_unicode_ci
+    volumes:
+      - db_moodle_data:/bitnami/mariadb
+    restart: unless-stopped
+volumes:
+    db_moodle_data:
+```
+
+1. Create a file `list_of_xmldb_files.txt` with the paths to all `install.xml` files of your moodle installation.
+You can do this under linux with the following command:
+```bash
+find /path/to/moodle -name install.xml > list_of_xmldb_files.txt
+```
+2. Run the script with `python3 convert_xmldb_to_create_table_statements.py > create_tables.sql` to create a file with all SQL CREATE TABLE statements.
+3. If the database `diagram` already exists (eg. from a previous run), drop it. `echo "DROP DATABASE diagram;" | mysql -h localhost -P 3312 -u root -pa`
+4. Create the database and create the tables as described in the `create_tables.sql` file: `echo "CREATE DATABASE IF NOT EXISTS diagram;" | mysql -h localhost -P 3312 -u root -pa && cat create_tables.sql | mysql -h localhost -P 3312 -u root -pa -D diagram`
+5. Now you can use any tool that can connect to a mariadb and create database diagrams from it (eg jetbrains IDEs or MySQL Workbench).
+
+Attention: Moodle has a lot of tables. You will have to create a selection of tables you want to show in the diagram.
---
+++ convert_xmldb_to_create_table_statements.py
@@ -0,0 +1,123 @@
+import xml.etree.ElementTree as ET
+import sys
+
+
+def apply_custom_fixes(sql_statements):
+    fixed_statements = []
+    for statement in sql_statements:
+        # Apply the fix to change FOREIGN KEY definition for gradeitemid
+        fixed_statement = statement.replace(
+            "FOREIGN KEY (gradeitemid, courseid) REFERENCES grade_items(id, courseid)",
+            "FOREIGN KEY (gradeitemid) REFERENCES grade_items(id)"
+        )
+        # Add more custom fixes here, if needed
+        # fixed_statement = fixed_statement.replace("some_old_text", "some_new_text")
+
+        fixed_statements.append(fixed_statement)
+    return fixed_statements
+
+# Convert XMLDB type to SQL type
+def get_sql_type(xmldb_type, length, decimals=0):
+    if xmldb_type == "int":
+        return f"INT({length})"
+    elif xmldb_type == "text":
+        return "TEXT"
+    elif xmldb_type == "char":
+        return f"VARCHAR({length})"
+    elif xmldb_type == "number":
+        return f"FLOAT({length},{decimals})" if decimals else f"FLOAT({length})"
+    elif xmldb_type == "float":
+        return "FLOAT"
+    else:
+        sys.stderr.write(f"Error: Unknown type '{xmldb_type}' encountered.\n")
+        sys.exit(1)
+
+# Function to handle the indexes
+def handle_indexes(indexes_element):
+    indexes = []
+    for index in indexes_element.findall('INDEX'):
+        index_name = index.get('NAME')
+        unique = 'UNIQUE' if index.get('UNIQUE') == "true" else ''
+        fields = index.get('FIELDS')
+        # Added backticks around index_name
+        indexes.append(f"{unique} INDEX `{index_name}` ({fields})")
+    return indexes
+
+# Add quotes around default values if they are strings
+def quote_default(default_value):
+    try:
+        int(default_value)
+        return default_value
+    except ValueError:
+        return f"'{default_value}'"
+
+# Read a list of XMLDB files and generate SQL CREATE TABLE statements
+def convert_xmldb_to_sql(file_list):
+    sql_statements = ["-- Disable foreign key checks\nSET foreign_key_checks = 0;"]
+
+    with open(file_list, 'r') as f:
+        files = f.readlines()
+
+    for file in files:
+        file = file.strip()
+        tree = ET.parse(file)
+        root = tree.getroot()
+
+        for table in root.find('TABLES').findall('TABLE'):
+            table_name = table.get('NAME')
+            comment = table.get('COMMENT', '')
+
+            fields = []
+            for field in table.find('FIELDS').findall('FIELD'):
+                field_name = field.get('NAME')
+                field_type = field.get('TYPE')
+                field_length = field.get('LENGTH', '')
+                field_notnull = "NOT NULL" if field.get('NOTNULL') == "true" else ""
+                field_default = f"DEFAULT {quote_default(field.get('DEFAULT'))}" if field.get('DEFAULT') else ""
+                field_auto_increment = "AUTO_INCREMENT" if field.get('SEQUENCE') == "true" else ""
+
+                field_decimals = field.get('DECIMALS', 0)
+                sql_type = get_sql_type(field_type, field_length, field_decimals)
+
+                field_line = f"{field_name} {sql_type} {field_notnull} {field_default} {field_auto_increment}".strip()
+                fields.append(field_line)
+
+            keys = []
+            for key in table.find('KEYS').findall('KEY'):
+                key_name = key.get('NAME')
+                key_type = key.get('TYPE').upper()
+                key_fields = key.get('FIELDS')
+                ref_table = key.get('REFTABLE')
+                ref_fields = key.get('REFFIELDS')
+                if key_type == "PRIMARY":
+                    keys.append(f"PRIMARY KEY ({key_fields})")
+                elif key_type == "UNIQUE":
+                    keys.append(f"UNIQUE ({key_fields})")  # Add this line to handle unique keys
+                elif ref_table and ref_fields:
+                    keys.append(f"FOREIGN KEY ({key_fields}) REFERENCES {ref_table}({ref_fields})")
+            # Handle Indexes
+            indexes_element = table.find('INDEXES')
+            if indexes_element is not None:
+                indexes = handle_indexes(indexes_element)
+                fields += indexes  # Append the index statements to the fields list
+
+
+            fields_and_keys = ",\n".join(fields + keys)
+            create_table_statement = f"CREATE TABLE {table_name} (\n{fields_and_keys}\n);"
+
+            if comment:
+                create_table_statement = f"-- {comment}\n" + create_table_statement
+
+            sql_statements.append(create_table_statement)
+
+    sql_statements = apply_custom_fixes(sql_statements)
+    sql_statements.append("-- Enable foreign key checks\nSET foreign_key_checks = 1;")
+    return sql_statements
+
+if __name__ == "__main__":
+    file_list = "list_of_xmldb_files.txt"  # Replace with your file that contains a list of XMLDB file paths
+    sql_statements = convert_xmldb_to_sql(file_list)
+
+    for sql in sql_statements:
+        print(sql)
+        print()
